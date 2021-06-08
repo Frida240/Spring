e7584f17e2cd0826507422b08f306de5b677775c
fuliming <fuliming@xiaomi.com>
2021-06-08 19:59:59 +0800

    Init

---
+++ src/main/java/sixpeople/SixpeopleApplication.java
@@ -0,0 +1,13 @@
+package sixpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixpeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixpeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sixpeople/SixpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package sixpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixpeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
